21c2875fdfc88be4ff5089064367d8c11deccaf9
joojimin <wnwlals22@naver.com>
2021-10-23 15:22:02 +0900

    feat(SimpleChunkJobConfiguration): 예제 작성 완료

--- src/main/java/com/toy/springbatch/job/chunk/SimpleChunkJobConfiguration.java
+++ src/main/java/com/toy/springbatch/job/chunk/SimpleChunkJobConfiguration.java
@@ -1,17 +1,20 @@
-package com.toy.springbatch.job;
+package com.toy.springbatch.job.chunk;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,7 +37,7 @@ public class SimpleChunkJobConfiguration {
     @Bean
     public Step simpleChunkStep() {
         return stepBuilderFactory.get("simpleChunkStep")
-                .<Integer, String>chunk(20)
+                .<UserDto, String>chunk(5)
                 .reader(flatFileReader())
                 .processor(simpleChunkProcessor())
                 .writer(simpleChunkWriter())
@@ -42,22 +45,38 @@ public class SimpleChunkJobConfiguration {
     }
 
     @Bean
-    public FlatFileItemReader<Integer> flatFileReader() {
-        return new FlatFileItemReaderBuilder<Integer>()
+    public FlatFileItemReader<UserDto> flatFileReader() {
+        return new FlatFileItemReaderBuilder<UserDto>()
                 .name("flatFileReader")
-                .resource(new FileSystemResource("./test.txt"))
-                .targetType(Integer.class)
+                .resource(new ClassPathResource("test.txt"))
+                .targetType(UserDto.class)
+                .lineMapper(lineMapper())
                 .build();
     }
 
     @Bean
-    public ItemProcessor<Integer, String> simpleChunkProcessor() {
-        return num -> String.format("accept processor => %d", num);
+    public LineMapper<UserDto> lineMapper() {
+        DefaultLineMapper<UserDto> lineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setNames("name");
+
+        BeanWrapperFieldSetMapper<UserDto> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(UserDto.class);
+
+        lineMapper.setLineTokenizer(lineTokenizer); // lineToken
+        lineMapper.setFieldSetMapper(fieldSetMapper);
+        return lineMapper;
+    }
+
+    @Bean
+    public ItemProcessor<UserDto, String> simpleChunkProcessor() {
+        return userDto -> String.format("accept processor => %s", userDto.getName());
     }
 
     @Bean
     public ItemWriter<String> simpleChunkWriter() {
-        return items -> log.info(String.join(",", items));
+        return items -> System.out.println(String.join(", ", items));
     }
 
 }
---
+++ src/main/java/com/toy/springbatch/job/chunk/UserDto.java
@@ -0,0 +1,8 @@
+package com.toy.springbatch.job.chunk;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String name;
+}
